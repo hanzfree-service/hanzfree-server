f1d9b296fdf0dda4b05b09e5bf4be9a2c9e20ae5
jerry <becomegoodguy@gmail.com>
2024-01-11 16:41:26 +0900

    feat: update httpOnly

--- src/auth/local-auth/local-auth.controller.ts
+++ src/auth/local-auth/local-auth.controller.ts
@@ -35,14 +35,9 @@ export class LocalAuthController {
     @Body() loginDto: LoginDto,
     @Res({ passthrough: true }) res: Response,
   ): Promise<any> {
-    console.log('hi');
-
     const user = await this.authService.validateUser(loginDto);
     const access_token = await this.authService.generateAccessToken(user);
     const refresh_token = await this.authService.generateRefreshToken(user);
-
-    console.log('access_token', access_token.slice(-10));
-    console.log('refresh_token', refresh_token.slice(-10));
 
     // refresh token DB에 저장
     const refreshTokenInfo = await this.userService.setCurrentRefreshToken(
@@ -52,14 +47,14 @@ export class LocalAuthController {
 
     res.setHeader('Authorization', 'Bearer ' + [access_token, refresh_token]);
     res.cookie('access_token', access_token, {
-      // httpOnly: true,
+      httpOnly: true,
       sameSite: 'none',
       secure: true,
       domain: '.hanzfree.co.kr',
       // secure: process.env.NODE_ENV === 'production', // HTTPS를 통해서만 쿠키 전송
     });
     res.cookie('refresh_token', refresh_token, {
-      // httpOnly: true,
+      httpOnly: true,
       sameSite: 'none',
       secure: true,
       domain: '.hanzfree.co.kr',
