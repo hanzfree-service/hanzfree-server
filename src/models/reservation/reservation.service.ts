825784dd70cab6026f5b72f128ba8583b37c4036
jerry <becomegoodguy@gmail.com>
2024-01-21 23:38:49 +0900

    feat: 예약 조회 API 및 예약 완료시 메일 발송 기능 추가

--- src/models/reservation/reservation.service.ts
+++ src/models/reservation/reservation.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateReservationDto } from './dto/create-reservation.dto';
 import { Reservation } from './entities/reservation.entity';
+import * as nodemailer from 'nodemailer';
+// import { createTransport, SendMailOptions } from 'nodemailer';
 
 @Injectable()
 export class ReservationService {
@@ -18,6 +20,8 @@ export class ReservationService {
     });
 
     const savedReservation = await this.reservationRepository.save(reservation);
+
+    await this.sendReservationConfirmationEmail(createReservationDto.email);
     return savedReservation;
   }
 
@@ -25,8 +29,55 @@ export class ReservationService {
     return this.reservationRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} reservation`;
+  async findOne(id: number): Promise<Reservation | undefined> {
+    const reservation = await this.reservationRepository.findOne({
+      where: { id: id },
+    });
+
+    if (!reservation) {
+      throw new NotFoundException(`Reservation with ID ${id} not found`);
+    }
+
+    return reservation;
+  }
+
+  async findAllByUserId(userId: number) {
+    return this.reservationRepository.find({ where: { user: { id: userId } } });
+  }
+
+  async findAllByUserIdAndReservationId(userId: number, reservationId: number) {
+    return this.reservationRepository.findOne({
+      where: { user: { id: userId }, id: reservationId },
+    });
+  }
+
+  async sendReservationConfirmationEmail(
+    email: string,
+    // reservation: CreateReservationDto,
+  ) {
+    // Nodemailer transporter 생성
+    const transporter = nodemailer.createTransport({
+      service: process.env.email_service,
+      auth: {
+        user: process.env.user,
+        pass: process.env.pass,
+      },
+    });
+
+    // 이메일 본문 작성
+    const emailBody = `
+      <h1>Thank you for your reservation!</h1>
+     
+      <p>If you have any questions, feel free to contact us.</p>
+    `;
+
+    // 이메일 전송
+    await transporter.sendMail({
+      from: process.env.user,
+      to: email,
+      subject: 'Reservation Confirmation',
+      html: emailBody,
+    });
   }
 
   // update(id: number, updateReservationDto: UpdateReservationDto) {
